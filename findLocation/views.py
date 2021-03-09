745c7b6bb563452bccabdc3ff8603c123f49c6d3
destinylim <dlim3@umbc.edu>
2021-03-09 17:43:29 -0500

    Google Maps API added and is able to find distance between hard coded locations

--- findLocation/views.py
+++ findLocation/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.sites import requests
 from django.http import HttpResponse
+from django.template import loader
+import json
+from urllib.parse import urlencode
+
+API_KEY = '***REMOVED***'
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('findLocation/index.html')
+    map_mode = 'place'
+    endpoint = f"https://www.google.com/maps/embed/v1/{map_mode}"
+    location = "Clarksburg, Maryland"
+    src = f"{endpoint}?key={API_KEY}&q={location}"
+    origin = 'Catonsville, Maryland'
+    destinations = ['Towson, Maryland']
+    return HttpResponse(template.render())#{'theUrl': src, 'origin': origin, 'destinations': destinations}))
+
+# def distance():
+#     base_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+#     origins = ['Vancouver,BC', 'Seattle']
+#     destination = ['San Francisco', 'Victoria, BC']
+#
+#     payload = {
+#         'origins': '|'.join(origins),
+#         'destinations': '|'.join(destination),
+#         'mode': 'driving',
+#         'api_key': API_KEY,
+#         'units': 'imperial'
+#     }
+#
+#     r = requests.GET(base_url, params=payload)
+#     return json.loads(r)

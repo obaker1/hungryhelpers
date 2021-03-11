d5ff1ade5f38d18ad6aa603091cf04109d5acb34
kristenloo <kloo2@umbc.edu>
2021-03-10 21:16:18 -0500

    findLocation:test and readme

--- findLocation/tests.py
+++ findLocation/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class PageLoad(TestCase):
+    # test findLocation page works
+    def test_page_load(self):
+        # access findLocation page
+        response = self.client.get('/findLocation/')
+        # verify site status code (HTTP 200 OK)
+        self.assertEqual(response.status_code, 200)
+        # verify index.html is being used
+        self.assertTemplateUsed(response, template_name='findLocation/index.html')

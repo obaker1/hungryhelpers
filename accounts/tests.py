f78a189d63e00b04e81d7a0de7a2521de2ea158f
obaker1 <obaker1@umbc.edu>
2021-03-10 23:03:10 -0500

    Added additional tests that verifies logout operation and unsuccessful logins

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.db import models
 
+
 class SignUpTest(TestCase):
     def setUp(self):
         self.username = 'test'
@@ -30,11 +31,18 @@ class SignUpTest(TestCase):
         users = get_user_model().objects.all()
         self.assertEqual(users.count(), 1)
 
+
 class LogInTest(TestCase):
     def setUp(self):
+        # valid credentials
         self.credentials = {
-           'username': 'test',
-           'password': '>pve_hm*N*&x<qbP8u'}
+            'username': 'test',
+            'password': '>pve_hm*N*&x<qbP8u'}
+        # invalid credentials
+        self.credentials2 = {
+            'username': 'test',
+            'password': '>pve_hm*N&x<qbP8u'
+        }
         User.objects.create_user(**self.credentials)
 
     def test_login_page(self):
@@ -45,9 +53,61 @@ class LogInTest(TestCase):
         # verify signup.html is being used
         self.assertTemplateUsed(response, template_name='registration/login.html')
 
-    def test_login(self):
+    def test_login_success(self):
+        # send login data
+        response = self.client.post('/accounts/login/', self.credentials, follow=True)
+        # check site redirection destination returns status code (HTTP 200 OK)
+        self.assertEqual(response.status_code, 200)
+        # check that the user successfully logged in
+        self.assertTrue(response.context['user'].is_active)
+        # check that the user has been redirected to home
+        self.assertTemplateUsed(response, template_name='home.html')
+
+    def test_login_fail(self):
+        # send login data
+        response = self.client.post('/accounts/login/', self.credentials2, follow=True)
+        # check site redirection destination returns status code (HTTP 200 OK)
+        self.assertEqual(response.status_code, 200)
+        # check that the user was unsuccessful when logging in
+        self.assertFalse(response.context['user'].is_active)
+        # check that the user is shown appropriate message
+        self.assertTrue('Please enter a correct username and password' in str(response.content))
+
+
+class LogOutTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'test',
+            'password': '>pve_hm*N*&x<qbP8u'}
+        User.objects.create_user(**self.credentials)
+
+    def test_logout_page(self):
+        # access logout page
+        response = self.client.get('/accounts/logout/')
+        # verify site redirection status code (HTTP 302)
+        self.assertEqual(response.status_code, 302)
+
+        # re-access logout page and follow redirection
+        response = self.client.get('/accounts/logout/', follow=True)
+        # verify site redirection destination returns status code (HTTP 200 OK)
+        self.assertEqual(response.status_code, 200)
+        # verify home.html is being used
+        self.assertTemplateUsed(response, template_name='home.html')
+
+    def test_logout(self):
         # send login data
         response = self.client.post('/accounts/login/', self.credentials, follow=True)
         # check that a page redirection occurred (HTTP 302)
-        #self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
+        # check that the user successfully logged in
         self.assertTrue(response.context['user'].is_active)
+        # check that the user has been redirected to home
+        self.assertTemplateUsed(response, template_name='home.html')
+        # request logout
+        response = self.client.get("/accounts/logout/", follow=True)
+        # check site status code (HTTP 200 OK)
+        self.assertEquals(response.status_code, 200)
+        # check user is no longer active
+        self.assertFalse(response.context['user'].is_active)
+        # check user is shown "You are not logged in" message
+        self.assertTrue('You are not logged in' in str(response.content))

44ac91fed3bbc15137976a035b163e80ecf2ca59
destinylim <dlim3@umbc.edu>
2021-03-25 00:25:49 -0400

    added destinations database

--- findLocation/views.py
+++ findLocation/views.py
@@ -4,8 +4,13 @@ from django.template import loader
 from django.shortcuts import render
 from urllib.parse import urlencode
 
-API_KEY = 'AIzaSyDE41t7LzAPR_yrfiqiJcCBEbw7YzUYJ5I'
+API_KEY = '***REMOVED***'
 
+from findLocation.models import Destinations
 
 def index(request):
+    destinations = Destinations.objects.filter()
+    context = {
+        'destinations' : destinations
+    }
     return render(request, 'findLocation/index.html', context={})

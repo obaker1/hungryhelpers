58aacdb6b5c4ff00998afb7a6edebf6c5fd9a762
destinylim <dlim3@umbc.edu>
2021-03-29 00:07:39 -0400

    All sensitive information hidden

--- findLocation/views.py
+++ findLocation/views.py
@@ -3,14 +3,15 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 from urllib.parse import urlencode
-
-API_KEY = '***REMOVED***'
+from django.conf import settings
 
 from findLocation.models import Destinations
+
 
 def index(request):
     destinations = Destinations.objects.filter()
     context = {
-        'destinations' : destinations
+        'destinations': destinations,
+        'api_key': settings.GOOGLE_MAPS_API_KEY
     }
-    return render(request, 'findLocation/index.html', context={})
+    return render(request, 'findLocation/index.html', context)
--- hungryhelpers/settings.py
+++ hungryhelpers/settings.py
@@ -12,22 +12,25 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n=(28+$&f^t)em5^@s=t16ke0n3q#d@jzzp0-cm)@o%d4&*p8i'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -57,7 +60,7 @@ ROOT_URLCONF = 'hungryhelpers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        #'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -73,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hungryhelpers.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -83,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -103,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -117,6 +117,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+GOOGLE_MAPS_API_KEY = env('GOOGLE_MAPS_API_KEY')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/

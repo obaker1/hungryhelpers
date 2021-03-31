a3b6acfcbc9740df4d6a4daab11a1dea90bc4d15
kristenloo <kloo2@umbc.edu>
2021-03-30 21:17:19 -0400

    Filter maps by school and bus stop

--- findLocation/static/findLocation/index.js
+++ findLocation/static/findLocation/index.js
@@ -1,22 +1,101 @@
+var map;
+var markersArray = [];
+var filters = {school:false, bus:false}; // start out with filter features set to false, so no filtering happens by default
+
+// constantly update filter markers
+$(function () {
+    $('input[name=filter]').change(function (e) {
+        map_filter(this.id);
+        filter_markers();
+    });
+})
+var map_filter = function(id_val) {
+   if (filters[id_val])
+      filters[id_val] = false;
+   else
+      filters[id_val] = true;
+}
+
+// get a subset of the filters that are set to true
+var get_set_options = function() {
+  ret_array = [];
+  for (option in filters) {
+    if (filters[option]) {
+      ret_array.push(option);
+    }
+  }
+  return ret_array;
+}
+
+var filter_markers = function() {
+  set_filters = get_set_options();
+  // for each marker, check to see if all required options are set
+  for (i = 0; i < markersArray.length; i++) {
+    marker = markersArray[i];
+    keep=true; // start the filter check assuming the marker will be displayed
+    for (opt=0; opt<set_filters.length; opt++) {
+      if (!marker.properties[set_filters[opt]]) {
+        keep = false; // if missing required feature
+      }
+    }
+    marker.setVisible(keep);
+  }
+}
+
+// loads the markers onto the map
+function loadMarkers() {
+  var infoWindow = new google.maps.InfoWindow();
+  geojson_url = 'https://raw.githubusercontent.com/obaker1/hungryhelpers/feature/filterMap/map.geojson';
+  // load the geoJSON file
+  $.getJSON(geojson_url, function(result) {
+      // Post select to url
+      data = result['features'];
+      // iterate through the map data to create markers
+      $.each(data, function(key, val) {
+        var point = new google.maps.LatLng(
+            parseFloat(val['geometry']['coordinates'][0]),
+            parseFloat(val['geometry']['coordinates'][1]));
+        var titleText = val['properties']['title'];
+        var descriptionText = val['properties']['description'];
+        var marker = new google.maps.Marker({
+          position: point,
+          title: titleText,
+          map: map,
+          properties: val['properties'],
+          label: "D"
+        });
+        //attachSecretMessage(marker, "Destination");
+        var markerInfo = "<div><h3>" + titleText + "</h3>Amenities: " + descriptionText + "</div>";
+
+        // show info windows when a marker is clicked
+        marker.addListener('click', function() {
+           $('#places_info').html(markerInfo);
+        });
+        markersArray.push(marker);
+      });
+  });
+}
+
 function initMap() {
   const bounds = new google.maps.LatLngBounds();
-  const markersArray = [];
     // locations and distances, hard coded for now
     const origin = "1000 Hilltop Circle Catonsville, Maryland";
-    const destinationA = "Towson, Maryland";
-    const destinationB = "Columbia, Maryland";
+    const destinationA = new google.maps.LatLng(39.401794462959664, -76.60364082158517);
+    const destinationB = new google.maps.LatLng(39.20393322969507, -76.85615843748513);
+    const destinationC = new google.maps.LatLng(39.321837620538545, -76.65404104179306);
 
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 55.53, lng: 9.4 },
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 39.332857, lng: -76.681437 },
     zoom: 10,
   });
+  loadMarkers(); // loads the markers onto the map
   const geocoder = new google.maps.Geocoder();
   const service = new google.maps.DistanceMatrixService();
   // creates distance matrix
   service.getDistanceMatrix(
     {
       origins: [origin],
-      destinations: [destinationA, destinationB],
+      destinations: [destinationA, destinationB, destinationC],
       travelMode: google.maps.TravelMode.DRIVING,
       unitSystem: google.maps.UnitSystem.METRIC,
       avoidHighways: false,
@@ -30,7 +109,6 @@ function initMap() {
         const destinationList = response.destinationAddresses;
         const outputDiv = document.getElementById("output");
         outputDiv.innerHTML = "";
-        deleteMarkers(markersArray);
 
         const showGeocodedAddressOnMap = function (asDestination) { //add markers on map
           const icon = asDestination ? "D" : "O";
@@ -38,13 +116,15 @@ function initMap() {
           return function (results, status) {
             if (status === "OK") { // makes sure that the google maps geoencoder loaded correctly
               map.fitBounds(bounds.extend(results[0].geometry.location));
-              const marker = new google.maps.Marker({
-                  map,
-                  position: results[0].geometry.location,
-                  label: icon,
-              })
-              markersArray.push(marker);
-              attachSecretMessage(marker, popup);
+                if (!asDestination) {
+                  const marker = new google.maps.Marker({
+                      map,
+                      position: results[0].geometry.location,
+                      label: icon,
+                  })
+                  markersArray.push(marker);
+                  attachSecretMessage(marker, popup);
+                }
               /*map.addListener("center_changed", () => { //testing map center change
                 // 3 seconds after the center of the map has changed, pan back to the marker.
                 window.setTimeout(() => {

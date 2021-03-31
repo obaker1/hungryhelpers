4d5e910d9bc4b6b088a1137a1775a2d2b0f7adcc
destinylim <dlim3@umbc.edu>
2021-03-31 18:16:30 -0400

    Tests are implemented

--- findLocation/tests.py
+++ findLocation/tests.py
@@ -33,20 +33,39 @@ class DestinationIndexViewTest(TestCase):
         """
         create_destination(destination_text="Towson, Maryland")
         response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Towson,")
 
-    # def test_order_of_locations(self):
-    #     """
-    #     makes sure that all locations in the database are ordered
-    #     correctly by distance (not by time)
-    #     """
+    def test_order_of_locations(self):
+        """
+        makes sure that all locations in the database are ordered
+        correctly by distance (not by time)
+        """
+        destinationList = ["Elkridge, MD", "Columbia, MD", "Towson, MD"]
+        for i in range(len(destinationList)):
+            create_destination(destination_text=destinationList[i])
+        response = self.client.get(reverse('index'))
+        counter = 0
+        for destinations in GoogleMapsResponse.objects.filter():
+            self.assertIn(destinationList[counter], destinations.location )
+            counter += 1
+
+        self.assertEqual(response.status_code, 200)
 
     def test_google_matrix_api(self):
         """
         makes sure that distance matrix API is working
         """
+        params = {
+            'key': settings.GOOGLE_MAPS_API_KEY,
+            'origins': ['Columbia, MD'],
+            'destinations': 'Towson, Maryland'
+        }
+        # calls distance matrix API
         url = 'https://maps.googleapis.com/maps/api/distancematrix/json?callback=initMap&libraries=&v=weekly&units=imperial'
-        response = requests.get(url)
+        response = requests.get(url, params)
+        origin_address = response.json()['origin_addresses'][0]
+        self.assertIn('Columbia, MD', origin_address)
         self.assertEqual(response.status_code, 200)
 
     def test_google_geocode_api(self):
@@ -56,4 +75,3 @@ class DestinationIndexViewTest(TestCase):
         url = 'https://maps.googleapis.com/maps/api/js?'
         response = requests.get(url)
         self.assertEqual(response.status_code, 200)
-

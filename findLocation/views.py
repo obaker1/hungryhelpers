6e63a369be027d0ee35c93bf63866c8881046b03
kristenloo <kloo2@umbc.edu>
2021-04-10 21:15:16 -0400

    Adding custom origin feature and removing locations from database feature

--- findLocation/views.py
+++ findLocation/views.py
@@ -12,7 +12,6 @@ import requests
 
 ORIGIN = '1000 Hilltop Cir, Baltimore, MD 21250, USA'
 
-
 def findlocation(request):
     destinationList = []
     filter = []
@@ -33,10 +32,46 @@ def findlocation(request):
     }
     return render(request, 'findLocation/index.html', context)
 
+# add custom origin from user
+def addOrigin(request):
+    global ORIGIN;
+    origin_text = request.POST['origin'];
+    if (origin_text != ''):
+        ORIGIN = origin_text;
+
+    for destination in GoogleMapsResponse.objects.all():
+        params = {
+            'key': settings.GOOGLE_MAPS_API_KEY,
+            'origins': ORIGIN,
+            'destinations': destination.location
+        }
+        url = 'https://maps.googleapis.com/maps/api/distancematrix/json?callback=initMap&libraries=&v=weekly&units=imperial'
+        response = requests.get(url, params)
+        try:
+            result = json.loads(response.text)
+        except json.decoder.JSONDecodeError:
+            result = "String could not be converted to JSON"
+
+        # if response is empty
+        if(result.get('status') != 'OK'):
+            return HttpResponseRedirect(reverse('findlocation'))
+
+        results = result['rows'][0]['elements'];
+        destinationList = result['destination_addresses']
+
+        location = destinationList[0]
+        distanceString = results[0]['distance']['text']
+        distanceString = distanceString.replace(',','');
+        distance = float(distanceString[:-3])
+        time = results[0]['duration']['text']
+        GoogleMapsResponse.objects.filter(location=location).update(distance=distance, time=time)
+
+    return HttpResponseRedirect(reverse('findlocation'))
 
 # finds the distances for the destination and stores it in the database
 def addLocation(request):
     destination_text = request.POST['destination']
+    remove = request.POST['remove']
     school_text = request.POST['school']
     school = "F"
     if (school_text == "y"):
@@ -57,17 +92,26 @@ def addLocation(request):
     except json.decoder.JSONDecodeError:
         result = "String could not be converted to JSON"
 
+    # if response is empty
+    if(result.get('status') != 'OK'):
+        return HttpResponseRedirect(reverse('findlocation'))
+
     results = result['rows'][0]['elements'];
     destinationList = result['destination_addresses']
 
     for i in range(len(destinationList)):
         location = destinationList[i]
         distanceString = results[i]['distance']['text']
+        distanceString = distanceString.replace(',','');
         distance = float(distanceString[:-3])
         time = results[i]['duration']['text']
-        if not GoogleMapsResponse.objects.filter(location=location, distance=distance).exists():
-            newResponse = GoogleMapsResponse(location=location, distance=distance, time=time, school=school, bus=bus)
-            newResponse.save()
+        if not GoogleMapsResponse.objects.filter(location=location).exists():
+            if (remove != 'r'):
+                newResponse = GoogleMapsResponse(location=location, distance=distance, time=time, school=school, bus=bus)
+                newResponse.save()
         else:
-            GoogleMapsResponse.objects.filter(location=location, distance=distance).update(school=school, bus=bus)
+            if (remove == 'r'):
+                GoogleMapsResponse.objects.filter(location=location).delete()
+            else:
+                GoogleMapsResponse.objects.filter(location=location).update(school=school, bus=bus)
     return HttpResponseRedirect(reverse('findlocation'))

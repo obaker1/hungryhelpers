2082e54fa74308376d295884c09795b20a89219b
LeeMega1 <leemega1@umbc.edu>
2021-04-14 13:47:40 -0400

    Created "Forgot Password" button when user is not logged on and "Change password" button when user is logged on. Added functionality to both buttons.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView, EditSettingsView, ShowProfileView, EditProfileView, CreateProfileView, AddStudentView
+from .views import SignUpView, EditSettingsView, ShowProfileView, EditProfileView, CreateProfileView, AddStudentView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
@@ -8,4 +8,12 @@ urlpatterns = [
     path('<int:pk>/profile/', ShowProfileView.as_view(), name='show_profile'),
     path('<int:pk>/edit_profile/', EditProfileView.as_view(), name='edit_profile'),
     path('<int:pk>/add_student/', AddStudentView.as_view(), name='add_student'),
+    # Path when user wants to change password (Needs to be logged on)
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # Path when user forgets password (Does not need to be logged on)
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset_form'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]

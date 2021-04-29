f1eced433777e0d8ae05304166ac140355ed67e9
destinylim <dlim3@umbc.edu>
2021-04-28 23:48:45 -0400

    tests added

--- mealPlan/tests.py
+++ mealPlan/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+import requests
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from findLocation.models import GoogleMapsResponse
+
 
 # Create your tests here.
+class PageLoad(TestCase):
+    # test findLocation page works
+    def test_page_load(self):
+        # access findLocation page
+        response = self.client.get('/mealPlan/staffPage/')
+        # verify site status code (HTTP 200 OK)
+        self.assertEqual(response.status_code, 200)
+        # verify index.html is being used
+        self.assertTemplateUsed(response, template_name='mealPlan/staffpage.html')
+
+
--- mealPlan/views.py
+++ mealPlan/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from findLocation.models import GoogleMapsResponse
 
 
-def index(request):
+def staffPage(request):
     googlemaps = GoogleMapsResponse.objects.all()
     context={'googlemapsresult': googlemaps}
     return render(request, 'mealPlan/staffpage.html', context)

24f5f0d5d8795bcdbe4f2e0bf72fc7304ef9a994
destinylim <dlim3@umbc.edu>
2021-04-29 16:21:15 -0400

    staff page html added

--- mealPlan/tests.py
+++ mealPlan/tests.py
@@ -28,12 +28,12 @@ class StaffPageLoadWithLocations(TestCase):
             num = results.filter(location=location).exists
             self.assertTrue(num)
 
-# class MealFilterPageLoad(TestCase):
-#     # test findLocation page works
-#     def test_page_load(self):
-#         # access findLocation page
-#         response = self.client.get('/mealPlan/choosemeal/')
-#         # verify site status code (HTTP 200 OK)
-#         self.assertEqual(response.status_code, 200)
-#         # verify index.html is being used
-#         self.assertTemplateUsed(response, template_name='mealPlan/choosemeal.html')
+class MealFilterPageLoad(TestCase):
+    # test findLocation page works
+    def test_page_load(self):
+        # access findLocation page
+        response = self.client.get('/mealPlan/choosemeal/')
+        # verify site status code (HTTP 200 OK)
+        self.assertEqual(response.status_code, 200)
+        # verify index.html is being used
+        self.assertTemplateUsed(response, template_name='mealPlan/choosemeal.html')

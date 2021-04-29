f107b56f5c56713bab13ff972239e93c584bcbed
destinylim <dlim3@umbc.edu>
2021-04-29 16:01:23 -0400

    tests added

--- mealPlan/tests.py
+++ mealPlan/tests.py
@@ -8,14 +8,29 @@ from findLocation.models import GoogleMapsResponse
 
 
 # Create your tests here.
-class PageLoad(TestCase):
+class PageLoadWithLocations(TestCase):
+    def setUp(self):
+        c = Client()
+        destinationList = ['Elkridge, MD', 'Towson, MD', 'Columbia, MD']
+        for i in destinationList:
+            c.post('/findLocation/addLocation/',
+                   {'destination': i, 'school': 'y', 'bus': 'n', 'timeframe': 'M\W', "remove": 'a'})
+
     # test findLocation page works
     def test_page_load(self):
-        # access findLocation page
+        # access staffPage page
         response = self.client.get('/mealPlan/staffPage/')
         # verify site status code (HTTP 200 OK)
         self.assertEqual(response.status_code, 200)
         # verify index.html is being used
         self.assertTemplateUsed(response, template_name='mealPlan/staffpage.html')
 
+    def test_adding_locations(self):
+        # access staffPage page
+        response = self.client.get('/mealPlan/staffPage/')
+        results = response.context['googlemapsresult']
+        destinationList = ['Elkridge, MD', 'Towson, MD', 'Columbia, MD']
+        for location in destinationList:
+            num = results.filter(location=location).exists
+            self.assertTrue(num)
 
